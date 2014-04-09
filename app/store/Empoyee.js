8b5495c2e9105c239260769a1da54ad74685f4ce
sifunea <sifunea@gmail.com>
2014-04-09 14:05:00 +0700

    Update Empoyee.js

--- app/store/Empoyee.js
+++ app/store/Empoyee.js
@@ -3,11 +3,130 @@ Ext.define('RM.model.Employee', {
     fields: [
       {
         name: 'Id',
-        type: 'Int'
+        type: 'int'
       },
       {
-        name: ''
-        
+        name: 'EmployeeNumber',
+        type: 'int'
+      },
+      {
+          name: 'FirstName',
+          type: 'string'
+      },
+      {
+          name: 'MiddleName',
+          type: 'string'
+      },
+      {
+          name: 'LastName',
+          type: 'string'
+      },
+      {
+          name: 'NationalityId',
+          type: 'int'
+      },
+      {
+          name: 'Nationality',
+          type: 'string'
+      },
+      {
+          name: 'DateOfBirth',
+          type: 'date',
+          dateWriteFormat: 'Y-m-d'
+      },
+      {
+          name: 'GenderId',
+          type: 'int'
+      },
+      {
+          name: 'Gender',
+          type: 'string'
+      },
+      {
+          name: 'MaritalStatusId',
+          type: 'int'
+      },
+      {
+          name: 'ssn',
+          type: 'int'
+      },
+      {
+          name: 'Nic',
+          type: 'int'
+      },
+      {
+          name: 'OtherId',
+          type: 'int'
+      },
+      {
+          name: 'DrivingLicenseNo',
+          type: 'int'
+      },
+      {
+          name: 'WorkStationId',
+          type: 'int'
+      },
+      {
+          name: 'AddressLine1',
+          type: 'string'
+      },
+      {
+          name: 'AddressLine2',
+          type: 'string'
+      },
+      {
+          name: 'City',
+          type: 'string'
+      },
+      {
+          name: 'Country',
+          type: 'string'
+      },
+      {
+          name: 'Province',
+          type: 'string'
+      },
+      {
+          name: 'Postal',
+          type: 'int'
+      },
+      {
+          name: 'HomePhone',
+          type: 'int'
+      },
+      {
+          name: 'MobilePhone',
+          type: 'int'
+      },
+      {
+          name: 'WorkPhone',
+          type: 'int'
+      },
+      {
+          name: 'WorkEmail',
+          type: 'string'
+      },
+      {
+          name: 'PrivateEmail'
+          type: 'string'
+      },
+      {
+          name: 'JoinedDate',
+          type: 'date',
+          dateWriteFormat: 'Y-m-d'
+      },
+      {
+          name: 'ConfirmationDate',
+          type: 'date',
+          dateWriteFormat: 'Y-m-d'
+      },
+      {
+          name: 'Department',
+          type: 'string'
+      },
+      {
+          name: 'Supervisor',
+          type: 'string'
       }
     ]
 });
