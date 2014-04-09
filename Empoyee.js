67b11c244266f2b69ad2fdfe39bf28c6322b6b4d
sifunea <sifunea@gmail.com>
2014-04-09 10:22:18 +0700

    Update Empoyee.js

--- Empoyee.js
+++ Empoyee.js
@@ -1,1 +1,13 @@
-ss
+Ext.define('RM.model.Employee', {
+    extend: 'RM.model.Base',
+    fields: [
+      {
+        name: 'Id',
+        type: 'Int'
+      },
+      {
+        name: ''
+        
+      }
+    ]
+});
